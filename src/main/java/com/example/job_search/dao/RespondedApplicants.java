febcf75f6c2aa4030d2a341e9449d0f6c456434d
Ergen <alymbekovergen7@gmail.com>
2026-03-19 04:31:08 +0600

    added annotation

--- src/main/java/com/example/job_search/dao/RespondedApplicants.java
+++ src/main/java/com/example/job_search/dao/RespondedApplicants.java
@@ -1,4 +1,10 @@
 package com.example.job_search.dao;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class RespondedApplicants {
 }

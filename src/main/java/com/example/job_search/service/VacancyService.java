86966cdddea991fcb3fbad00060f7c2e98d1f118
Ergen <alymbekovergen7@gmail.com>
2026-03-23 10:43:13 +0600

    change model to dto

--- src/main/java/com/example/job_search/service/VacancyService.java
+++ src/main/java/com/example/job_search/service/VacancyService.java
@@ -1,14 +1,14 @@
 package com.example.job_search.service;
 
-import com.example.job_search.model.Vacancies;
+import com.example.job_search.dto.VacanciesDto;
 
 import java.util.List;
 
 public interface VacancyService {
-    List<Vacancies> getAllVacancies();
-    void createVacancy(Vacancies vacancy);
-    void updateVacancy(int id, Vacancies vacancy);
+    List<VacanciesDto> getAllVacancies();
+    void createVacancy(VacanciesDto vacanciesDto);
+    void updateVacancy(int id, VacanciesDto vacanciesDto);
     void deleteVacancy(int id);
-    List<Vacancies> getByCategory(int categoryId);
-    List<Vacancies> getRespondedByUser(int applicantId);
+    List<VacanciesDto> getByCategory(int categoryId);
+    List<VacanciesDto> getRespondedByUser(int applicantId);
 }

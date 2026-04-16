8bcad86f746e734cafb736cae497adcd0c2e0c93
Ergen <alymbekovergen7@gmail.com>
2026-04-16 18:18:52 +0600

    forgot to add annotation

--- src/main/java/com/example/job_search/repository/ResumeRepository.java
+++ src/main/java/com/example/job_search/repository/ResumeRepository.java
@@ -2,6 +2,8 @@ package com.example.job_search.repository;
 
 import com.example.job_search.model.Resumes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResumeRepository extends JpaRepository<Resumes, Integer> {
 }
--- src/main/java/com/example/job_search/repository/VacancyRepository.java
+++ src/main/java/com/example/job_search/repository/VacancyRepository.java
@@ -2,6 +2,8 @@ package com.example.job_search.repository;
 
 import com.example.job_search.model.Vacancies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VacancyRepository extends JpaRepository<Vacancies, Integer> {
 }

9d721953763efe3187ae447704ef32d3c925a496
Ergen <alymbekovergen7@gmail.com>
2026-04-21 21:50:01 +0600

    fix bugs

--- src/main/java/com/example/job_search/config/SecurityConfig.java
+++ src/main/java/com/example/job_search/config/SecurityConfig.java
@@ -73,8 +73,13 @@ public class SecurityConfig {
 
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 
+                        //Вакансии читаю все
                         .requestMatchers(HttpMethod.GET, "/vacancies").permitAll()
                         .requestMatchers(HttpMethod.GET, "/vacancies/**").permitAll()
+
+                        // а резюме только полсе авторизации
+                        .requestMatchers(HttpMethod.GET, "/resumes").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/resumes/**").authenticated()
                         
                         .requestMatchers(HttpMethod.POST, "/resumes").hasAuthority("APPLICANT")
                         .requestMatchers(HttpMethod.PUT, "/resumes/**").hasAuthority("APPLICANT")
@@ -83,6 +88,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/vacancies").hasAuthority("EMPLOYER")
                         .requestMatchers(HttpMethod.PUT, "/vacancies/**").hasAuthority("EMPLOYER")
                         .requestMatchers(HttpMethod.DELETE, "/vacancies/**").hasAuthority("EMPLOYER")
+
+                        //profile смотрят только авториз
+                        .requestMatchers( "/profile/**").authenticated()
 
                         .anyRequest().permitAll()
                 );

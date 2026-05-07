7495cc01ac2d3ab56fc14ccd86df0b6c3b7f7c66
Ergen <alymbekovergen7@gmail.com>
2026-05-07 17:15:14 +0600

    add LocaleConfig

---
+++ src/main/java/com/example/job_search/config/LocaleConfig.java
@@ -0,0 +1,33 @@
+package com.example.job_search.config;
+
+
+
+
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localResolver(){
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+}
